f5d1de679973e1f4975c602b35ffc697fd1090bb
Marwangustiandruru <91463883+Marwangustiandruru@users.noreply.github.com>
2021-09-30 20:51:36 +0700

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,27 @@
+package products;
+
+public class Product {
+    String name;
+    double price = 100000;
+    private double subtotal, discount_amount, tax_amount, grandTotal;
+    public Product(String name){
+        this.name = name;
+
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void getProductListing(int qty, double tax, double discount){
+            subtotal = qty*price;
+            discount_amount = discount/100 * subtotal;
+            tax_amount = tax/100 * subtotal;
+            grandTotal = subtotal - discount + tax;
+        System.out.println("==========ProductListing========");
+        System.out.println(String.format("Harga dalam Rupiah: Rp%,.2f", price));
+        System.out.println(String.format("Subtotal: Rp%,.2f", subtotal));
+        System.out.println(String.format("potongan: Rp%,.2f", discount_amount));
+        System.out.println(String.format("pajak: Rp%,.2f", tax_amount));
+        System.out.println(String.format("grand total: Rp%,.2f", grandTotal));
+        System.out.println("==================");
+    }
+}
